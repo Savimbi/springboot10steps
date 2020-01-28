802e294fe5ae5f3cbc3a89102950cd365a1906ea
savimbi <kig@lI18>
2020-01-28 12:00:19 +0900

    Starting point of springboot in 10 steps

---
+++ src/main/java/com/dudosa/springboot/basics/SpingbootIn10stepsApplication.java
@@ -0,0 +1,13 @@
+package com.dudosa.springboot.basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingbootIn10stepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingbootIn10stepsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dudosa/springboot/basics/SpingbootIn10stepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dudosa.springboot.basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingbootIn10stepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
